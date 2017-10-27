9ddad653108f59ba9b926d8b7e18717e8e0a8a8a
fasol228 <promakc31@gmail.com>
2017-10-28 06:24:01 +0800

    update back [work]

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,25 @@ stmt = database.prepare( "insert into users( login ) values( ? )" );
 stmt.run( "FroZo" );
 stmt.finalize();*/
 
-/*app.use(function (req, res, next) {
+
+
+// Формирование ответа сервера
+// -- tp: success/error
+// -- message: сообщение
+function getResponse( tp, message ){
+  return { "type" : tp, "message" : message };
+}
+
+// Точка входа
+function initialization(){
+  // Подключаем преобразование данных в JSON
+  // Подключаемся к провайдеру ethereum в локальной сети
+  server.use( bodyToJson.json() );
+  server.use( bodyToJson.urlencoded({
+      extended: true
+  }))
+  web3.setProvider( new web3.providers.HttpProvider( "http://localhost:8545" ) );
+  server.use(function (req, res, next) {
     var origins = [
         'http://localhost:3000',
     ];
@@ -33,41 +51,20 @@ stmt.finalize();*/
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});*/
-
-// Формирование ответа сервера
-// -- tp: success/error
-// -- message: сообщение
-function getResponse( tp, message ){
-  return { "type" : tp, "message" : message };
-}
-
-// Точка входа
-function initialization(){
-  // Подключаем преобразование данных в JSON
-  // Подключаемся к провайдеру ethereum в локальной сети
-  server.use( bodyToJson.json() );
-  web3.setProvider( new web3.providers.HttpProvider( "http://localhost:8545" ) );
-  
+});
   // Обработчик POST запроса
   server.post( "/", function( req, res, next ){
     var response;
     
-    console.log( req.body );
-    
     // Проверяем, есть ли "event" в данных
     try{
       evnt = req.body[ "event" ];
-      
-      console.log( evnt );
       
       // На разный "event" разный ответ
       switch( evnt ){
         // Получить баланс пользователя в сети ethereum
         // через хэш аккаунта
         case "get balance":
-          console.log( "get balance" );
-          
           try{
             response = getResponse( "success", web3.eth.getBalance( req.body[ "account" ] ).c[0] );
           } catch( err ){ response = getResponse( "error", "Invalid account address" ); };
@@ -85,7 +82,7 @@ function initialization(){
   } );
   
   // Запускаем сервер
-  server.listen( 8020, "192.168.10.208", function() {
+  server.listen( 8020, function() {
     console.log( "Our server on 8020 port" );
   } );
 }
